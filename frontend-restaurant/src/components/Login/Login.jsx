a73c490c55e5c195dae601ee67c9779c3ae1e9b5
Dilushan <gunalandilushan@gmail.com>
2025-01-29 18:48:49 +0530

    user authentication

--- frontend-restaurant/src/components/Login/Login.jsx
+++ frontend-restaurant/src/components/Login/Login.jsx
@@ -1,28 +1,67 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Login.css'
 import { assets } from '../../assets/assets'
+import { StoreContext } from '../../context/StoreContext'
+import axios from 'axios'
 
 const Login = ({setShowLogin}) => {
 
-    const [currState,setCurrState] = useState("Sign Up")
+  const {url,setToken} = useContext(StoreContext)
+
+    const [currState,setCurrState] = useState("Login")
+
+    const [data,setData] = useState({
+      name:"",
+      email:"",
+      password:""
+    })
+
+    const onChangeHandler = (event) =>{
+      const name = event.target.name;
+      const value = event.target.value;
+      setData(data=>({...data,[name]:value}))
+    }
+
+    const onLogin = async (event) => {
+      event.preventDefault()
+      let newUrl = url;
+      if (currState==="Login") {
+        newUrl += "/api/user/login"
+      }
+      else{
+        newUrl += "/api/user/register"
+      }
+
+      const response = await axios.post(newUrl,data);
+      if (response.data.success) {
+        setToken(response.data.token)
+        localStorage.setItem("token",response.data.token)
+        setShowLogin(false)
+      }
+      else{
+        alert(response.data.message)
+      }
+    }
+
+
 
   return (
     <div className='login'> 
-      <form className="login-container">
+      <form onSubmit={onLogin} className="login-container">
         <div className="login-title">
             <h2>{currState}</h2>
             <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
         </div>
         <div className="login-inputs">
-            {currState==="Login"?<></>:<input type="text" placeholder='Your name' required/>}
-            <input type="email" placeholder='Your email address' required/>
-            <input type="password" placeholder='password' required />
+            {currState==="Login"?<></>:<input name='name' onChange={onChangeHandler} value={data.name} type="text" placeholder='Your name' required/>}
+            <input name='email' onChange={onChangeHandler} value={data.email} type="email" placeholder='Your email address' required/>
+            <input name='password' onChange={onChangeHandler} value={data.password} type="password" placeholder='password' required />
         </div>
-        <button>{currState==="Sign Up"?"Create Account":"Login"}</button>
+        <button type='submit'>{currState==="Sign Up"?"Create Account":"Login"}</button>
         <div className="login-condition">
-            <input type="checkbox" />
+            <input type="checkbox" required/>
             <p>By clicking, I agree to the terms of use & Privacy policy.</p>
         </div>
         {currState==="Login"
